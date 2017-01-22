7e2818741e85aab5fbdb8e239b40f033a7677555
Adam Jones <jones_adam@rocketmail.com>
2017-01-22 23:03:29 +0000

    Create store items in javascript

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 e_cookie = document.getElementById('cookie');
 e_cookieQuantity = document.getElementById('cookieQuantity');
 e_cookieQuantity_under = document.getElementById('cookieQuantity_under');
+e_products = document.getElementById('products');
 
 // Create eventlistener on cookie
 e_cookie.addEventListener('click', cookieClick);
 
-// Start with 0 cookies
+// Start with 0 cookies and empty storeOptions object
 var cookies = 0;
+var storeOptions = {};
 
 // User clicks the giant cookie
 function cookieClick() {
@@ -20,9 +22,32 @@ function cookieClick() {
 // Display cookies
 function displayCookies() {
     if(cookies == 1) {
-        e_cookieQuantity_under.innerHTML = "cookie";
+        e_cookieQuantity_under.innerHTML = 'cookie';
     } else {
-        e_cookieQuantity_under.innerHTML = "cookies";
+        e_cookieQuantity_under.innerHTML = 'cookies';
     }
     e_cookieQuantity.innerHTML = cookies.toLocaleString();
 }
+
+function addStoreOption(storeOption) {
+    var s = "<li class='product' id='" + storeOption.id + "'>" +
+            "<p class='product_owned'>" + ((storeOption.product_owned === 0) ? "" : storeOption.product_owned) + "</p>" +
+            "<img src='" + storeOption.image_src + "' alt='" + storeOption.product_name +"' class='product_image'  width='50px' height='50px'>" +
+            "<div class='product_text'>" +
+            "<p class='product_title'><span class='product_name'>" + storeOption.product_name + "</span> (<span class='product_cps'>" + storeOption.product_cps.toLocaleString() + "</span> cps)</p>" +
+            "<p class='product_price'>" + storeOption.product_price.toLocaleString() + "</p></li>" +
+            "</div>";
+    e_products.innerHTML += s;
+}
+
+// Creates cursor storeOption object
+storeOptions["1"] = {
+    id: 1,
+    product_owned: 3,
+    image_src: "cursor.svg",
+    product_name: "Cursor",
+    product_cps: 0.1,
+    product_price: 10
+};
+
+addStoreOption(storeOptions["1"]);
